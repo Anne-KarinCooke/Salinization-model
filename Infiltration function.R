a5aed6d9818f3ab3d5182edfe5453787f7064376
Anne-KarinCooke <anne-karin.cooke@sydney.edu.au>
2016-10-17 16:36:15 +1100

    Delete Infiltration function.R

--- Infiltration function.R
+++
@@ -1,8 +0,0 @@
-# INFILTRATION FUNCTION ##########################################################################
-
-Infil <- function(h,P, alpha_i=0.25, k=12, W0=0.2){
-  
-  I=alpha_i*h*(P+k*W0)/(P+k)
-  
-  return (I)
-}
